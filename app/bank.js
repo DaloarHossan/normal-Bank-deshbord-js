e25e2e46a564945b155e3e00705d7e4fd8f0e3eb
Daloar Hossan <daloarhossan128@gmail.com>
2022-02-11 12:57:14 +0600

    click-tone

--- app/bank.js
+++ app/bank.js
@@ -1,145 +1,156 @@
-document.getElementById('deposit-button').addEventListener('click',function(){
-	const deposit=document.getElementById('deposit-ammount');
-	const newdepositAmmount=deposit.value;
-	const depositTotal=document.getElementById('deposit-total');
-	const previousdepositTotal=depositTotal.innerText;
-	const total=document.getElementById('balance');
-	const previousBalance=total.innerText;
+document.getElementById('deposit-button').addEventListener('click', function () {
+	const deposit = document.getElementById('deposit-ammount');
+	const newdepositAmmount = deposit.value;
+	const depositTotal = document.getElementById('deposit-total');
+	const previousdepositTotal = depositTotal.innerText;
+	const total = document.getElementById('balance');
+	const previousBalance = total.innerText;
 
-   if(isNaN(newdepositAmmount)){
-	  const p= document.createElement('small');
-	   p.innerText='please enter a valid ammount!'
-	   p.style.color='red';
-	   const errorP=document.getElementById('error');
-	   errorP.appendChild(p);
-   }
-   else{
-	const totalDeposit=parseFloat(newdepositAmmount)+parseFloat(previousdepositTotal);
-	depositTotal.innerText=totalDeposit;
-	const totalbalance =parseFloat(newdepositAmmount)+parseFloat(previousBalance);
-	total.innerText=totalbalance;
-	 deposit.value=null;
- 
-   }
-}) 
-document.getElementById('plus').addEventListener('click',function(){
-	const deposit=document.getElementById('deposit-ammount');
-	const depositAmmount=deposit.value;
-	if(isNaN(depositAmmount)){
-		const p= document.createElement('small');
-		 p.innerText='please enter a valid ammount!'
-		 p.style.color='red';
-		 const errorP=document.getElementById('error');
-		 errorP.appendChild(p);
-	 }
-	 else{
-		const deposit=document.getElementById('deposit-ammount');
-		const depositAmmount=deposit.value;
+	if (isNaN(newdepositAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const totalDeposit = parseFloat(newdepositAmmount) + parseFloat(previousdepositTotal);
+		depositTotal.innerText = totalDeposit;
+		const totalbalance = parseFloat(newdepositAmmount) + parseFloat(previousBalance);
+		total.innerText = totalbalance;
+		deposit.value = null;
+
+	}
+})
+
+document.getElementById('plus').addEventListener('click', function () {
+	audioClick();
+	const deposit = document.getElementById('deposit-ammount');
+	const depositAmmount = deposit.value;
+	if (isNaN(depositAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const deposit = document.getElementById('deposit-ammount');
+		const depositAmmount = deposit.value;
 		deposit.value++;
-		
-	 }
+
+	}
 
 })
 
 // minus
 
-document.getElementById('minus').addEventListener('click',function(){
-	const deposit=document.getElementById('deposit-ammount');
-	const depositAmmount=deposit.value;
-	if(isNaN(depositAmmount)){
-		const p= document.createElement('small');
-		 p.innerText='please enter a valid ammount!'
-		 p.style.color='red';
-		 const errorP=document.getElementById('error');
-		 errorP.appendChild(p);
-	 }
-	 else{
-		const deposit=document.getElementById('deposit-ammount');
-		const depositAmmount=deposit.value;
-		if(depositAmmount>=0)
-		{
+document.getElementById('minus').addEventListener('click', function () {
+	audioClick();
+	const deposit = document.getElementById('deposit-ammount');
+	const depositAmmount = deposit.value;
+	if (isNaN(depositAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const deposit = document.getElementById('deposit-ammount');
+		const depositAmmount = deposit.value;
+		if (depositAmmount >= 0) {
 			deposit.value--;
 		}
-		else{
-			deposit.value=0;
+		else {
+			deposit.value = 0;
 		}
-		
-	 }
+
+	}
 
 })
 
 // withdraw 
 
-document.getElementById('withdraw-button').addEventListener('click',function(){
-	const withdraw=document.getElementById('withdraw-ammount');
-	const newwithdrawAmmount=withdraw.value;
-	const withdrawTotal=document.getElementById('withdraw-total');
-	const previouswithdrawTotal=withdrawTotal.innerText;
-	const total=document.getElementById('balance');
-	const previousBalance=total.innerText;
+document.getElementById('withdraw-button').addEventListener('click', function () {
+	const withdraw = document.getElementById('withdraw-ammount');
+	const newwithdrawAmmount = withdraw.value;
+	const withdrawTotal = document.getElementById('withdraw-total');
+	const previouswithdrawTotal = withdrawTotal.innerText;
+	const total = document.getElementById('balance');
+	const previousBalance = total.innerText;
 
-   if(isNaN(newwithdrawAmmount)){
-	  const p= document.createElement('small');
-	   p.innerText='please enter a valid ammount!'
-	   p.style.color='red';
-	   const errorP=document.getElementById('error');
-	   errorP.appendChild(p);
-   }
-   else{
-	const totalwithdraw=parseFloat(newwithdrawAmmount)+parseFloat(previouswithdrawTotal);
-	withdrawTotal.innerText=totalwithdraw;
-	const totalbalance =parseFloat(previousBalance)-parseFloat(newwithdrawAmmount);
-	total.innerText=totalbalance;
-	 withdraw.value=null;
- 
-   }
-}) 
-document.getElementById('plusW').addEventListener('click',function(){
-	const withdraw=document.getElementById('withdraw-ammount');
-	const withdrawAmmount=withdraw.value;
-	if(isNaN(withdrawAmmount)){
-		const p= document.createElement('small');
-		 p.innerText='please enter a valid ammount!'
-		 p.style.color='red';
-		 const errorP=document.getElementById('error');
-		 errorP.appendChild(p);
-	 }
-	 else{
-		const withdraw=document.getElementById('withdraw-ammount');
-		const depositAmmount=withdraw.value;
+	if (isNaN(newwithdrawAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const totalwithdraw = parseFloat(newwithdrawAmmount) + parseFloat(previouswithdrawTotal);
+		withdrawTotal.innerText = totalwithdraw;
+		const totalbalance = parseFloat(previousBalance) - parseFloat(newwithdrawAmmount);
+		total.innerText = totalbalance;
+		withdraw.value = null;
+
+	}
+})
+document.getElementById('plusW').addEventListener('click', function () {
+
+	audioClick();
+	const withdraw = document.getElementById('withdraw-ammount');
+	const withdrawAmmount = withdraw.value;
+	if (isNaN(withdrawAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const withdraw = document.getElementById('withdraw-ammount');
+		const depositAmmount = withdraw.value;
 		withdraw.value++;
-		
-	 }
+
+	}
 
 })
 
 // minus
 
-document.getElementById('minusW').addEventListener('click',function(){
-	const withdraw=document.getElementById('withdraw-ammount');
-	const withdarwAmmount=withdraw.value;
-	if(isNaN(withdarwAmmount)){
-		const p= document.createElement('small');
-		 p.innerText='please enter a valid ammount!'
-		 p.style.color='red';
-		 const errorP=document.getElementById('error');
-		 errorP.appendChild(p);
-	 }
-	 else{
-		const withdarw=document.getElementById('withdarw-ammount');
-		const withdrawAmmount=withdraw.value;
-		if(withdrawAmmount>=0)
-		{
+document.getElementById('minusW').addEventListener('click', function () {
+	audioClick();
+	const withdraw = document.getElementById('withdraw-ammount');
+	const withdarwAmmount = withdraw.value;
+	if (isNaN(withdarwAmmount)) {
+		const p = document.createElement('small');
+		p.innerText = 'please enter a valid ammount!'
+		p.style.color = 'red';
+		const errorP = document.getElementById('error');
+		errorP.appendChild(p);
+	}
+	else {
+		const withdarw = document.getElementById('withdarw-ammount');
+		const withdrawAmmount = withdraw.value;
+		if (withdrawAmmount >= 0) {
 			withdraw.value--;
 		}
-		else{
-			withdraw.value=0;
+		else {
+			withdraw.value = 0;
 		}
-		
-	 }
+
+	}
 
 })
-	
-   
 
-   
+
+// click audio 
+function audioClick() {
+	const audio = new Audio();
+	audio.src = "audio/click.mp3"
+	audio.play();
+}
+
+
+
